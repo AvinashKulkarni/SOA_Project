3cc9fbb40b5b7a5af71b4d31dcf016a1156836ff
avinash <avinashmk6@gmail.com>
2016-10-06 13:09:31 +0530

    First commit

---
+++ ManufacturerMSRabbitMQ/src/main/java/com/example/ManufacturerMsRabbitMqApplication.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient // this will make a micro service register itself with config server
+public class ManufacturerMsRabbitMqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManufacturerMsRabbitMqApplication.class, args);
+	}
+}
---
+++ SpringDataRest/src/main/java/com/example/controller/MyFirstController.java
@@ -0,0 +1,18 @@
+package com.example.controller;
+
+import java.util.Calendar;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyFirstController {
+
+	@RequestMapping(value = "/", method=RequestMethod.GET)
+	public String greet() {
+		Calendar calendar = Calendar.getInstance();
+		int hour = calendar.get(Calendar.HOUR_OF_DAY);
+		return (hour < 12) ? "Goor Morning" : "Good Afternoon";
+	}
+}
